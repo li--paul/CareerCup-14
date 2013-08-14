a57cd64631861c16d8a00170f6224270668709a8
Xiaorui Zhang <xrayzh0728@gmail.com>
2013-08-14 14:39:49 +0800

    Update and rename solution_5_1.h to solution_5_1.cpp

--- solution_5_1.cpp
+++ solution_5_1.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 
 int setBits (int n, int m, int i, int j) {
 	int left = (~0)<<j;
@@ -5,4 +6,11 @@ int setBits (int n, int m, int i, int j) {
 	int mask = left | right;
 	
 	return (n&mask)|(m<<i);
+}
+
+int main() {
+	int N = 0x400;
+	int M = 0x015;
+	cout << setBits(N,M,2,6) << endl;
+	return 0;
 }
