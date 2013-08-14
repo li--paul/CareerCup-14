b1b41dcb2b705863af41b25b2b439148bf856e78
Xiaorui Zhang <xrayzh0728@gmail.com>
2013-08-14 13:50:12 +0800

    Update solution_1_7.h

--- solution_1_7.h
+++ solution_1_7.h
@@ -1,9 +1,10 @@
+#include <iostream>
 
 int printMatrix(int* matrix, int m, int n) {
 	if (!matrix) return 0;
 	for (int i=0;i<m;i++) {
 		for (int j=0;j<n;j++)
-			std::cout << matrix[i*n+j] << " ";
+			std::cout << matrix[i*n+j] << "\t";
 		std::cout << std::endl;
 	}
 	return 1;
@@ -25,4 +26,12 @@ int setZeros(int* matrix, int m, int n) {
 		for (int j=0;j<n;j++)
 			if (row[i]||col[j]) matrix[i*n+j] = 0;
 	return 1;
+}
+
+int main() {
+	int matrix[16] = {1,2,3,4,0,6,7,8,9,0,11,12,13,14,0,16};
+	printMatrix(matrix,4,4);
+	setZeros(matrix,4,4);
+	printMatrix(matrix,4,4);
+	return 0;
 }
