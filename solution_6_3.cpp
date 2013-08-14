d135668d2ea2280f452a5f8b434727f3236ab141
Xiaorui Zhang <xrayzh0728@gmail.com>
2013-08-14 14:53:12 +0800

    Create solution_6_3.cpp

---
+++ solution_6_3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+        cout << "(5):0 (3):0" << endl;
+        cout << "(5):5 (3):0" << endl;
+        cout << "(5):2 (3):3" << endl;
+        cout << "(5):2 (3):0" << endl;
+        cout << "(5):0 (3):2" << endl;
+        cout << "(5):5 (3):2" << endl;
+        cout << "(5):4 (3):3" << endl;
+}
