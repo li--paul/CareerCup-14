a42dcffe2536331f69a5b02f9298cc08f2852488
Xiaorui Zhang <xrayzh0728@gmail.com>
2013-08-14 14:28:09 +0800

    Update and rename solution_5_6.h to solution_5_6.cpp

--- solution_5_6.cpp
+++ solution_5_6.cpp
@@ -1,6 +1,13 @@
+#include <iostream>
 
 int swapBits (int n) {
 	int odd = 0xAAAAAAAA;
 	int even = 0x55555555;
 	return ((n&odd)>>1) | ((n&even)<<1);
+}
+
+int main() {
+	int num = 5;
+	cout << swapBits(num);
+	return 0;
 }
