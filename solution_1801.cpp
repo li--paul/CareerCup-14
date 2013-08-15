ea538c87b1a090f6f40c77facedf123dc3e8afdc
Xiaorui Zhang <xrayzh0728@gmail.com>
2013-08-15 14:45:24 +0800

    Create solution_1801.cpp

---
+++ solution_1801.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+        cout << "Processes do not share resources." << endl;
+        cout << "Threads do share resources." << endl;
+        cout << "One process can have multiple threads." << endl;
+        return 0;
+}
