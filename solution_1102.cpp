f2d5fc3d8201133d636b5c16eb9a66c2807fbf66
Xiaorui Zhang <xrayzh0728@gmail.com>
2013-08-15 12:46:13 +0800

    Create solution_1102.cpp

---
+++ solution_1102.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+        cout << "Random variables" << endl;
+        cout << "Memory leakage" << endl;
+        cout << "System dependency" << endl;
+        
+        return 0;
+}
