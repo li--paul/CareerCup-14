ba3b1ce758a68faef8519ca37556008704ccfae1
Xiaorui Zhang <xrayzh0728@gmail.com>
2013-08-16 11:13:38 +0800

    Create solution_1502.cpp

---
+++ solution_1502.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+        cout << "Inner Join" << endl;
+        cout << "Outter Join" << endl;
+        cout << "- Left Join" << endl;
+        cout << "- Right Join" << endl;
+        cout << "- Full Outer Join" << endl;
+        return 0;
+}
