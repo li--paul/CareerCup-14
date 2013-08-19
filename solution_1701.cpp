55d0f0653ee4d043890a7d257017929b562b9eda
Xiaorui Zhang <xrayzh0728@gmail.com>
2013-08-19 15:55:13 +0800

    Create solution_1701.cpp

---
+++ solution_1701.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+        std::cout << "Browser sends domain name to the DNS server." << std::endl;
+        std::cout << "Browser receives IP address from the DNS server." << std::endl;
+        std::cout << "Browser opens TCP connection to the web server at the IP address through port 80." << std::endl;
+        std::cout << "Browser sends HTTP request and receives HTML codes." << std::endl;
+        std::cout << "Browser renders the HTML page in a window." << std::endl;
+}
